9b33bd225cab765b97f88e0904fd3ef2880dc0ce
serlus <serluscasas@gmail.com>
2020-09-23 07:06:31 -0300

    install pytest

--- portfolio/core/views.py
+++ portfolio/core/views.py
@@ -1,7 +1,7 @@
-# from django.shortcuts import render
 from django.http import HttpResponse
+# from django.shortcuts import render
 
 
 # Create your views here.
 def home(request):
-    return HttpResponse('home')
+    return HttpResponse('Home')
